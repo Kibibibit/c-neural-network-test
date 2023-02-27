34d88be866983ba679bc3b4e4b346a6977325fe7
Dan M <daniel@thewindmills.com.au>
2023-02-28 10:02:40 +1100

    Fixed constants

--- constants.h
+++ constants.h
@@ -1,11 +1,11 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-#define TRUE 1;
-#define FALSE 0;
+#define TRUE 1
+#define FALSE 0
 
-#define EXIT_SUCCESS 0;
-#define EXIT_ASSERT_FAILED 2;
+#define EXIT_SUCCESS 0
+#define EXIT_ASSERT_FAILED 2
 
 
 #endif
