be652cc2d937075dcc4cf5d1f65b34416529a2c7
Dan M <daniel@thewindmills.com.au>
2023-02-28 10:02:06 +1100

    Updated assert to use constants

--- helpers.c
+++ helpers.c
@@ -2,10 +2,11 @@
 #include <stdio.h>
 #include "helpers.h"
 #include "typedefs.h"
+#include "constants.h"
 
 void assert(boolean expression, char * message) {
     if (!expression) {
         printf(message);
-        exit(1);
+        exit(EXIT_ASSERT_FAILED);
     }
 }
